0a538cca7586b981be0a40511fb04990bc0ed13e
ReshmaRangaraj <2326kb39@kgcas.com>
2025-06-06 14:37:05 +0530

    Add files via upload

---
+++ Day1/Task1.java
@@ -0,0 +1,44 @@
+package Day1;
+
+import java.util.*;
+
+public class Task1 {
+	
+
+	public static void main(String[] args) {
+
+
+	Scanner in=new Scanner(System.in);
+
+	System.out.println("Enter amount:");
+
+
+	int amt=in.nextInt();
+
+	System.out.println("Enter pin number:");
+
+	int pin=in.nextInt();
+
+	if(pin==1234) {
+
+	if(amt<=5000) {
+
+	System.out.println("withdrawn successfully...");
+
+	}else {
+
+	System.err.println("insufficient amount...");
+
+	}
+
+	}else{
+
+	System.out.println("invalid pinnumber...");
+
+	}
+
+
+
+	}}
+
+
