0fd617a7fdc179cafecd65abca0c65b25d9d7aa5
ReshmaRangaraj <2326kb39@kgcas.com>
2025-06-09 11:38:22 +0530

    Add files via upload

---
+++ Task/Task1.java
@@ -0,0 +1,13 @@
+package Task;
+	 import java.util.Scanner;
+	
+public class Task1 {
+	public static void main(String[]args) {
+		Scanner Scanner =new Scanner(System.in);
+		int [] n = {1,2,3,4,5,6};
+				for(int i=n.length-1;i>=0;i--)
+					System.out.println(i);
+	   }
+	}
+
+
---
+++ Task/Task3.java
@@ -0,0 +1,26 @@
+package Task;
+	
+import java.util.Scanner;
+  //SplitWords 
+public class Task3 {
+	
+		    public static void main(String[] args) {
+
+		        Scanner in = new Scanner(System.in);
+
+		        System.out.print("Enter a sentence: ");
+
+		        String sentence = in.nextLine();
+
+		        String[] words = sentence.split(" ");
+
+		        System.out.println("Words in the sentence:");
+
+		        for (String word : words) {
+
+		            System.out.println(word);
+
+		        }
+	}
+
+}
