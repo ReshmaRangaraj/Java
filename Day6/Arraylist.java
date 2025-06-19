f87156d36ade3c43ac790495652ddb1595dca1d1
ReshmaRangaraj <2326kb39@kgcas.com>
2025-06-19 14:40:55 +0530

    Update and rename Task1.java to Arraylist.java

---
+++ Day6/Arraylist.java
@@ -0,0 +1,24 @@
+package Day6;
+// array list:
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+public class Task1 {
+	public static void main(String[] args) {
+		ArrayList<Object> arr=new ArrayList<Object>();
+		arr.add("ABC");
+		arr.add("B");
+		arr.add("Computer Technology");
+		arr.add("Final Year");
+		arr.add(0,"DEF");
+		arr.get(2);
+		arr.set(4,"Kg");
+		arr.remove(2);
+		arr.add(3,"Web Develop");
+		for(Object data:arr){
+			System.out.println(data);
+		}
+		
+	}
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
